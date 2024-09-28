89a6575ff399bf27749e0dc28194093ee8e88e1a
Hossein <hosseinabedinipoco@gmail.com>
2024-09-28 18:49:45 +0330

    create random short code generator

--- urlShorter/app/serializer.py
+++ urlShorter/app/serializer.py
@@ -1,15 +1,32 @@
 from rest_framework import serializers
 from .models import Url
+import random
+import string
+from django.db import IntegrityError
+
 
 class UrlSerializer(serializers.ModelSerializer):
     class Meta:
         model = Url
         fields = '__all__'
         
+
+    def generate_short_code(self):
+        # Generate a code with 3 letters and 3 digits
+        letters = ''.join(random.choices(string.ascii_letters, k=3))
+        digits = ''.join(random.choices(string.digits, k=3))
+        return letters + digits
+        
     def create(self, validated_data):
-        random_code = 1
-        url = Url.objects.create(
-            url = validated_data['url'],
-            short_code = random_code
-        )    
+        while True:
+            random_code = self.generate_short_code()
+            try:
+                url = Url.objects.create(
+                    url=validated_data['url'],
+                    short_code=random_code
+                )
+                break
+            except IntegrityError:
+                continue
+        
         return url
