e1fb41226be70a4136d9a5c4de9e35198bf0de00
Hossein <hosseinabedinipoco@gmail.com>
2024-09-28 18:20:23 +0330

    add url patterns

--- urlShorter/app/views.py
+++ urlShorter/app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import APIView
 # Create your views here.
+
+class create_url(APIView):
+    def post(self, request):
+        pass
+
+class opreate_url(APIView):
+    def get(self, request, code):
+        pass
+
+    def put(self, request, code):
+        pass
+
+    def delete(self, request, code):
+        pass
+
+
