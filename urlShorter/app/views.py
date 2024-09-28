ec415f8518212f4855c93ff7158361bc25e042ff
Hossein <hosseinabedinipoco@gmail.com>
2024-09-28 18:40:09 +0330

    implement the create url view

--- urlShorter/app/views.py
+++ urlShorter/app/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from rest_framework.response import Response
 from rest_framework.decorators import APIView
+from .models import Url
+from .serializer import UrlSerializer
+from rest_framework import status
 # Create your views here.
 
 class create_url(APIView):
     def post(self, request):
-        pass
+        url = UrlSerializer(data=request.data)
+        if url.is_valid():
+            url.save()
+            return Response(url.data, status=status.HTTP_201_CREATED)
+        return Response(url.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class opreate_url(APIView):
     def get(self, request, code):
