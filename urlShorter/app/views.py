95498a95596525a1bfe6a6ac135838d882bc8341
Hossein <hosseinabedinipoco@gmail.com>
2024-09-28 18:55:01 +0330

    implement oprations on urls

--- urlShorter/app/views.py
+++ urlShorter/app/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import APIView
 from .models import Url
 from .serializer import UrlSerializer
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 class create_url(APIView):
@@ -17,12 +18,23 @@ class create_url(APIView):
 
 class opreate_url(APIView):
     def get(self, request, code):
-        pass
+        url = get_object_or_404(Url, short_code=code)
+        serializer = UrlSerializer(url)
+        return Response(url.data, status=status.HTTP_200_OK)
+
 
     def put(self, request, code):
-        pass
+        url = get_object_or_404(Url, short_code=code)
+        serializer = UrlSerializer(url, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
     def delete(self, request, code):
-        pass
+        url = get_object_or_404(Url, short_code=code)
+        url.delete()
+        return Response({'message':"deleted"}, status=status.HTTP_204_NO_CONTENT)
 
 
